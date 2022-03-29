7db90a30506e0f32cbeb14f051fc60f78ea097c0
Emin <90508482+AimlessCode@users.noreply.github.com>
2022-03-29 20:52:24 +0300

    Add files via upload

--- gameManeger.cs
+++ gameManeger.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class gameManeger : MonoBehaviour
 {
     public  GameObject gameovertype;
@@ -25,6 +26,11 @@ public class gameManeger : MonoBehaviour
         gameovertype.SetActive(true);
 
         Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
---
+++ score.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+
+    public static int Score =0;
+    void Start()
+    {
+        Score =0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<UnityEngine.UI.Text>().text=Score.ToString();
+    }
+}
